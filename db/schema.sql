73a991cfc964cc50ea6267a5b0031e117a599e08
TheBiggestThereIs <107923914+EthanDMello@users.noreply.github.com>
2022-11-09 23:58:58 +0000

    added schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,31 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_db;
+
+CREATE TABLE category (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    category_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE product (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    product_name VARCHAR(30) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock INT NOT NULL DEFAULT 10,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+CREATE TABLE tag (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    tag_name VARCHAR(30)
+);
+
+CREATE TABLE product_tag(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    product_id INT,
+    tag_id INT,
+    FOREIGN KEY (product_id) REFERENCES product(id),
+    FOREIGN KEY (tag_id) REFERENCES tag(id)
+)
